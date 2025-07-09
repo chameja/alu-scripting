1e92cc55a90727734a319e37c0eae1e75eb63fad
Chipo Hameja <c.hameja@alustudent.com>
2025-07-09 19:11:01 +0200

    commit

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""DOCS"""
-import requests
+
+""" place holder """
 
 
 def top_ten(subreddit):
-    """Docs"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    headers = headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()['data']
-        for post in data['children'][:10]:
-            print(post['data']['title'])
-    else:
-        print(None)
+    import requests
+    link = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    result = requests.get(link, headers={"User-Agent": "Mozilla/5.0"})
+    result = result.json()
+    try:
+        result = result["data"]["children"][:10]
+        for x in result:
+            print("{}".format(x["data"]["title"]))
+    except Exception as error:
+        print("None")
