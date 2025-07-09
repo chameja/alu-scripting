05338a9a88c7dfecdcd2fed2464d02a6a32d3890
Chipo Hameja <c.hameja@alustudent.com>
2025-07-09 19:06:12 +0200

    commit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -6,11 +6,18 @@ place holder
 
 def number_of_subscribers(subreddit):
     import requests
-    req = requests.get("https://api.reddit.com/r/{}/about.json"
-                       .format(subreddit),
-                       headers={"User-Agent": "Mozilla/5.0"})
-    req = req.json()
-    try:
-        return req["data"]["subscribers"]
-    except Exception as error:
-        return 0
+
+    """DOC"""
+    reddit_url = "https://www.reddit.com/r/{}/about.json" \
+        .format(subreddit)
+
+    header = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url,
+                            headers=header
+                            )
+
+    if response.status_code == 200:
+        data = response.json()['data']
+        subs = data['subscribers']
+        return subs
+    return 0
